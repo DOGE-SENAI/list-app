8ac93828b6f10e9bdfec545767fe6845ac4a9ebe
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2021-10-22 19:45:28 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [item, setItem] = useState('');
+  
+  const [itemList, setItemList] = useState([]);
+
+  const addItem = () => {
+    setItemList([item].concat(itemList))
+    setItem('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+		<div className="App">
+			<h1>Lista de Compras</h1>
+
+      <div className="container-form">
+        <label>Adicione um item</label>
+        <input
+          type="text"
+          placeholder="Item"
+          value={item}
+          name="item"
+          onChange={(e) => setItem(e.target.value)}
+        />
+			  <button onClick={addItem}>Adicionar Item</button>
+      </div>
+
+      <div className="container-list">
+        <ul>
+          {itemList.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      </div>
+      
+      <footer>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          By <a href="https://github.com/CauaRodrigues" target="_blank" rel="noreferrer">Cauã Rodrigues</a> | <a href="https://github.com/DOGE-SENAI" target="_blank" rel="noreferrer">DOGE</a> © 2021  
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </footer>
+		</div>
+	);
 }
 
 export default App;
